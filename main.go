d6a2bec8bfe4f6ce2f2e3c5d5a233a8887181054
David Bates <me@dave-bates.net>
2025-03-22 22:38:13 -0500

    Adds command struvture

--- main.go
+++ main.go
@@ -1,15 +1,68 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
 func cleanInput(text string) []string {
-	stringVals := strings.Fields(text)
+	stringVals := strings.Fields(strings.ToLower(text))
 	return stringVals
 }
 
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!\nUsage:\n\nhelp: Displays a help message\nexit: Exit the Pokedex")
+	return nil
+}
+
 func main() {
-	fmt.Println(cleanInput("Hello, World!"))
+	commands := map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exits the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Shows this help message",
+			callback:    commandHelp,
+		},
+	}
+	scanner := bufio.NewScanner(os.Stdin)
+	
+	for {
+		fmt.Print("Pokedex > ")
+
+		scanner.Scan()
+		input := cleanInput(scanner.Text())
+
+		if len(input) == 0 {
+			continue
+		}
+
+		cmd, ok := commands[input[0]]
+		fmt.Println(input)
+		if !ok {
+			fmt.Printf("Command \"%s\" not found.\n", input[0])
+		} else {
+			err := cmd.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+		}
+	}
 }
